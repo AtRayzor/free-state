f4272c0a155087750e34ddf48943aa355d5953d3
Tim Ray <timothy@timray.dev>
2026-02-01 19:33:42 -0600

    chore: remove redundant configuration
    Remark:
    The sourcemap tsconfig setting is already set to true.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   workspace: true,
   exports: true,
-  sourcemap: false,
   minify: true,
   dts: true,
-  outDir: "dist"
+  outDir: "dist",
 })
